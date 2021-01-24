243241d1328bdc8cd977f83c6b58e213e107a50e
hikalium <hikalium@hikalium.com>
2021-01-24 23:22:23 +0900

    Use bootstrap

--- index.js
+++ index.js
@@ -27,17 +27,18 @@ function sendMessage(s) {
 }
 </script>
 <style>
-button {
-  height: 100px;
-  width: 100px;
-}
 </style>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
 <body>
+<div class="container">
 <h1>slack-event-logger</h1>
 ${
-      terms.map(s => `<button onclick="sendMessage('${s}');">${s}</button>`)
+      terms.map(s => `<p><button type="button" class="btn btn-primary" onclick="sendMessage('${s}');">${s}</button></p>`)
           .join('\n')}
+</div>
 </body>
     `);
 });
