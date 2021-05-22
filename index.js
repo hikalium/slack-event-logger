b1d5eda5fda1a690be6c757a4109e9c5e0a1a2cc
hikalium <hikalium@hikalium.com>
2021-05-23 01:18:23 +0900

    Update design to improve accesibility

--- index.js
+++ index.js
@@ -33,12 +33,16 @@ app.get('/', (req, res) => {
   <meta charset="utf-8">
   <title>slack-event-logger</title>
 <script src="/socket.io/socket.io.js"></script>
-<style>
-</style>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<style>
+.btn-success {
+    border: 4px solid #8cfd0c;
+    border-right: 16px solid #8cfd0c;
+}
+</style>
 </head>
 <body>
 <div class="container">
